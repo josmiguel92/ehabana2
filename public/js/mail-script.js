b562eb131065596a05c039a57c61a1c9d137ebe0
Josue Miguel <josmiguel92@gmail.com>
2019-09-25 19:07:38 -0400

    adding js n css

---
+++ public/js/mail-script.js
@@ -0,0 +1,31 @@
+    // -------   Mail Send ajax
+
+     $(document).ready(function() {
+        var form = $('form.bookform-form'); // contact form
+        var submit = $('button.submit-btn'); // submit button
+        var alert = $('.alert-msg'); // alert div for show alert message
+
+        // form submit event
+        form.on('submit', function(e) {
+            e.preventDefault(); // prevent default form submit
+
+            $.ajax({
+                url: form[0].action, // form action url
+                type: 'POST', // form submit method get/post
+                dataType: 'html', // request type html/json/xml
+                data: form.serialize(), // serialize form data
+                beforeSend: function() {
+                    alert.fadeOut();
+                    submit.html('Sending....'); // change submit button text
+                },
+                success: function(data) {
+                    alert.html(data).fadeIn(); // fade in response data
+                    form.hide(); // reset form
+                    //submit.attr("style", "display: none !important");; // reset submit button text
+                },
+                error: function(e) {
+                    console.log(e)
+                }
+            });
+        });
+    });
