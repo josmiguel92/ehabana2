09caaf5d59bedec752bad246ea36e8ad31223175
Josue Miguel <josmiguel92@gmail.com>
2019-09-25 19:13:19 -0400

    adding assets

---
+++ public/assets/0.62f1f43ce9531ac9b7b3.hot-update.js
@@ -0,0 +1,16 @@
+webpackHotUpdate(0,{
+
+/***/ "../../../../../../scripts/frontend.js":
+/*!***************************************************!*\
+  !*** E:/Work/ehabana2/assets/scripts/frontend.js ***!
+  \***************************************************/
+/*! no exports provided */
+/*! all exports used */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("Object.defineProperty(__webpack_exports__, \"__esModule\", { value: true });\n/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__styles_frontend_scss__ = __webpack_require__(/*! ../styles/frontend.scss */ \"../../../../../../styles/frontend.scss\");\n/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__styles_frontend_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__styles_frontend_scss__);\n\n\n//todo..... rendimiento en moviles es malo\n//import './frontend/scrollTransformImage/index.js'//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi4vLi4vLi4vLi4vLi4vLi4vc2NyaXB0cy9mcm9udGVuZC5qcy5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy9FOi9Xb3JrL2VoYWJhbmEyL2Fzc2V0cy9zY3JpcHRzL2Zyb250ZW5kLmpzPzI5NjciXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0ICcuLi9zdHlsZXMvZnJvbnRlbmQuc2Nzcyc7XG5cbi8vdG9kby4uLi4uIHJlbmRpbWllbnRvIGVuIG1vdmlsZXMgZXMgbWFsb1xuLy9pbXBvcnQgJy4vZnJvbnRlbmQvc2Nyb2xsVHJhbnNmb3JtSW1hZ2UvaW5kZXguanMnXG5cblxuLy8vLy8vLy8vLy8vLy8vLy8vXG4vLyBXRUJQQUNLIEZPT1RFUlxuLy8gRTovV29yay9laGFiYW5hMi9hc3NldHMvc2NyaXB0cy9mcm9udGVuZC5qc1xuLy8gbW9kdWxlIGlkID0gLi4vLi4vLi4vLi4vLi4vLi4vc2NyaXB0cy9mcm9udGVuZC5qc1xuLy8gbW9kdWxlIGNodW5rcyA9IDAiXSwibWFwcGluZ3MiOiJBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///../../../../../../scripts/frontend.js\n");
+
+/***/ })
+
+})
